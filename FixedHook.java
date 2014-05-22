694ddae643a329cc5f11cd434ad9178a137feead
Gonzalo Sanchez <gonza@tuix.cl>
2014-05-22 01:18:38 -0400

    Etapa 3 funcional

--- FixedHook.java
+++ FixedHook.java
@@ -5,15 +5,22 @@ public class FixedHook extends PhysicsElement implements SpringAttachable
 {
 	private static int id = 0;
 	private double pos_t;
+	private double width;
 	private FixedHookView view;
 	private ArrayList<Spring> springs;
 
-	public FixedHook(double pos_t)
+	public FixedHook(double pos_t, double width)
 	{
 		super(id++);
 		this.pos_t = pos_t;
+		this.width = width;
 		this.view = new FixedHookView(this);
 		this.springs = new ArrayList<Spring>();
+	}
+
+	public double getWidth()
+	{
+		return this.width;
 	}
 
 	/* Implementacion de PhysicsElement */
