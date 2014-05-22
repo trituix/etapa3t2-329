0a12255cff2326deba8cd5c2c0efd57a54396474
Gonzalo Sanchez <gonza@tuix.cl>
2014-05-22 00:43:10 -0400

    Arreglos para que compile

--- FixedHookView.java
+++ FixedHookView.java
@@ -2,10 +2,19 @@ import java.awt.*;
 import java.awt.geom.*;
 
 public class FixedHookView {
-   private final double width;  // fixed point width
-   private Color color = Color.GREEN;
-   private Rectangle2D.Double shape = null;
-   private FixedHook hook;
+	private final double width;
+	private Color color = Color.GREEN;
+	private Rectangle2D.Double shape = null;  
+	private FixedHook hook;
 
-/* to be coded */
+	public FixedHookView(FixedHook hook)
+	{
+	   	this.hook = hook;
+	   	this.width = 5;
+	}
+
+	void updateView(Graphics2D g)
+	{
+
+	}
 }
